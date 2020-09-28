3c6197bce9c14454dadb2489a541a32058488967
codertyler <codertylerasai@gmail.com>
2020-09-28 23:42:14 +0000

    added the random number generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT =  8080; // default port
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -18,6 +21,10 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.req.params.longURL };
   res.render('urls_show', templateVars);
@@ -32,3 +39,12 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString () {
+  let result = Math.random().soString(36).substring(6);
+  return result;
+};
