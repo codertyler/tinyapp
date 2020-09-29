451d012a76cfe362b19926792741c3fd1345756f
codertyler <codertylerasai@gmail.com>
2020-09-29 21:11:34 +0000

    added the updating part in the server

--- express_server.js
+++ express_server.js
@@ -44,6 +44,14 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 })
 
+app.post('/urls/:shortURL/update', (req, res) =>{
+  
+
+  urlDatabase[req.params.shortURL] = req.body.newURL ;
+  
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
