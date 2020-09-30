403140f561448c64d8c01f4fad94ddedeae3ff55
codertyler <codertylerasai@gmail.com>
2020-09-30 02:26:40 +0000

    added logout feature

--- express_server.js
+++ express_server.js
@@ -21,7 +21,12 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls : urlDatabase };
+  const templateVars = { 
+    urls : urlDatabase,
+    username: req.cookies["username"],
+    };
+
+  
   res.render('urls_index', templateVars);
 });
 
@@ -55,7 +60,13 @@ app.post('/urls/:shortURL/update', (req, res) =>{
 
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
+  res.redirect('/urls');
 });
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
 
 
 app.listen(PORT, () => {
