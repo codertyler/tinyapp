604e83c64f24e46c3295d1649b3d3475b073023f
codertyler <codertylerasai@gmail.com>
2020-09-30 16:12:15 +0000

    initial login page inputs

--- express_server.js
+++ express_server.js
@@ -68,6 +68,10 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 })
 
+app.get('/login_form', (req, res) => {
+  res.redirect('/login_form');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
